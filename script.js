58be24f7976edd66d33cd289f7a9c33ec1d22d22
aniketjadhav6134 <164015482+aniketjadhav6134@users.noreply.github.com>
2024-04-04 21:54:07 +0530

    First Commit
    It can be used in add to cart option.

---
+++ script.js
@@ -0,0 +1,32 @@
+const display = document.querySelector('#display');
+let errorBox = document.getElementById('errorBox');
+let Value = display.value;
+const increament=()=>{  
+    Number(Value);
+    Value++;
+    display.value = Value;
+    
+}
+
+const decreament=()=>{
+    Number(Value);
+    if(0 >= Value){
+        displayErr();
+    }
+    else{
+        Value--;
+        display.value = Value;
+    }
+}
+
+const displayErr = () =>{
+    let Error = document.createElement('div');
+    Error.classList.add('error');
+    Error.innerHTML = "Error: Value must be greater than ZERO!";
+    errorBox.appendChild(Error);
+
+    setTimeout(()=>{
+        Error.remove();
+    },3000);
+}
+
